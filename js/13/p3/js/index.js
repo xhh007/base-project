7f84773b71a79f43a8309a5df7ab8b64dc8d3c77
zhangxiahui <liwenlong@thunisedu.com>
2026-02-11 17:51:13 +0800

    fix: dom练习

--- js/13/p3/js/index.js
+++ js/13/p3/js/index.js
@@ -1,15 +1,42 @@
 /**
  * 根据协议的多选框是否选中设置注册按钮状态
  */
-function setSubmitButtonStatus() {}
+function setSubmitButtonStatus() {
+  var inputCb = document.querySelector('input[type=checkbox]')
+  var btn = document.querySelector('button[type=submit]')
+  if (inputCb.checked) {
+    btn.disabled = false
+  } else {
+    btn.disabled = true
+  }
+}
 
 /**
  * 根据手机号文本框中的文本，设置发送验证码按钮的状态
  */
-function setSendCodeButtonStatus() {}
+function setSendCodeButtonStatus() {
+  const phoneRegex = /^1[3-9]\d{9}$/;
+  const str = document.querySelector('#tel').value
+  console.log(str);
+  
+  const vaild = phoneRegex.test(str);
+  var btn = document.querySelector('#testCode')
+  btn.disabled = !vaild
+}
 
 /**
  * 根据当前选中的爱好，设置已选择爱好文本
  */
-function setSelectedLoves() {}
+function setSelectedLoves() {
+  const options = document.getElementsByTagName('option') 
+  let arr = [].slice.call(options) 
+  //const options = document.querySelectorAll('select option')
+  const span = document.querySelector('#showLoves')
+  let str = '';
+  
+  arr.forEach(o => {
+    if (o.selected) str += o.label
+  })
+  span.innerHTML = str
+}
 
